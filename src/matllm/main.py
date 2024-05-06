cfc4e12a532f46fc189598b861b7c1d5f935d688
JosePizarro3 <jose.pizarro@physik.hu-berlin.de>
2024-05-06 18:57:42 +0200

    Added generate function

--- src/matllm/main.py
+++ src/matllm/main.py
@@ -2,12 +2,11 @@ import httpx
 from ollama import Client
 
 host = 'http://172.28.105.30/backend'
-model = 'llama3:70b'
-
 client = Client(host=host)
 # ollama client doesn't handle the /backend properly
 client._client = httpx.Client(base_url=host, timeout=500)
 
 
-response = client.generate(model=model, prompt='Why is the sky blue')
-print(response.get('response'))
+def generate(model: str = 'llama3:70b', prompt: str = ''):
+    response = client.generate(model=model, prompt=prompt)
+    return response.get('response')
--- src/matllm/prompt.py
+++ src/matllm/prompt.py
@@ -1,17 +1,20 @@
 from pydantic import BaseModel
-from langchain_community.llms.ollama import Ollama
+
+from .main import generate
 
 
-class Site(BaseModel):
-    atom: str
-    coordinates: list[float]
+# class Site(BaseModel):
+#     atom: str
+#     coordinates: list[float]
 
 
-class System(BaseModel):
-    system_id: int
-    sites: list[Site]
-    lattice: list[list[float]]
-    energy: float
+# class System(BaseModel):
+#     system_id: int
+#     sites: list[Site]
+#     lattice: list[list[float]]
+#     energy: float
 
 
-schema_dict = {'type': 'array', 'items': System.model_json_schema()}
+# schema_dict = {'type': 'array', 'items': System.model_json_schema()}
+
+print(generate(prompt='Why the sky is blue?'))
