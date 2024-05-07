14c3c06ebc66972151097c682b8185dd76b3aa68
Ahmed Ilyas <ahmed.n.ilyas@gmail.com>
2024-05-07 10:41:12 +0200

    Add langchain (#9)
    * add more langchain dependencies
    
    * add langchain example
    
    * remove ollama dependency
    
    * refactor: move to llm to llm_model file
    
    * add more packages

--- src/llmad/main.py
+++ src/llmad/main.py
@@ -1,13 +1,5 @@
-import httpx
-from ollama import Client
+from llmad.llm_model import llm
 
-host = 'http://172.28.105.30/backend'
-model = 'llama3:70b'
+response = llm.invoke('Tell me a joke')
 
-client = Client(host=host)
-# ollama client doesn't handle the /backend properly
-client._client = httpx.Client(base_url=host, timeout=500)
-
-
-response = client.generate(model=model, prompt='Why is the sky blue')
-print(response.get('response'))
+print(response)
