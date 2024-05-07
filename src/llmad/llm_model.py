d80e409d4827cd453ca5b2e17bf2e13fa4af7d0f
JosePizarro3 <jose.pizarro@physik.hu-berlin.de>
2024-05-07 09:03:19 +0200

    Added prompt_query function in main

--- src/llmad/llm_model.py
+++ src/llmad/llm_model.py
@@ -1,4 +1,29 @@
-from langchain_community.llms import Ollama
+from typing import Optional, List
+
+from langchain_core.pydantic_v1 import BaseModel, Field
+from langchain_community.llms.ollama import Ollama
 
 llm = Ollama(model='llama3:70b')
 llm.base_url = 'http://172.28.105.30/backend'
+
+
+class Actor(BaseModel):
+    """
+    Information about the actor appearing as a character in the movie.
+    """
+
+    name: Optional[str] = Field(..., description='The name of the actor.')
+    role: Optional[str] = Field(
+        ..., description='The name of the character that the actor interpreted.'
+    )
+
+
+class Casting(BaseModel):
+    """
+    Information about the casting of the movie.
+    """
+
+    # director: Optional[str] = Field(
+    #     ..., description='The name of the director of the movie.'
+    # )
+    actors: List[Actor]
--- src/llmad/main.py
+++ src/llmad/main.py
@@ -1,5 +1,46 @@
-from llmad.llm_model import llm
+from typing import Optional
 
-response = llm.invoke('Tell me a joke')
+from langchain_core.pydantic_v1 import BaseModel
+from langchain_community.llms.ollama import Ollama
+from langchain.output_parsers import PydanticOutputParser
+from langchain_core.prompts import ChatPromptTemplate
 
-print(response)
+from llmad.llm_model import llm, Casting
+
+
+def prompt_query(
+    query: str = '', schema: BaseModel = Casting, llm: Ollama = llm
+) -> Optional[BaseModel]:
+    """
+    Prompt the user with a `query` and return the output as a Pydantic object whose Fields are populated by the `llm` parsing.
+
+    Args:
+        query (str, optional): The string to be parsed into the `pydantic_object`. Defaults to ''.
+        schema (BaseModel, optional): The `BaseModel` with Fields to be populated. Defaults to None. Defined in the `llm_model.py` module.
+        llm (Ollama, optional): The LLM used for parsing. Defaults to `Ollama`.
+
+    Returns:
+        (Optional[BaseModel]): The populated `BaseModel`.
+    """
+    if schema is None:
+        raise ValueError('`schema` must be provided to be a defined `BaseModel`.')
+    parser = PydanticOutputParser(pydantic_object=schema)
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            (
+                'system',
+                'Answer the user query. Wrap the output in ```json``` tags\n{format_instructions}',
+            ),  # TODO improve this prompting to be specific for our cases
+            ('human', '{query}'),
+        ]
+    ).partial(format_instructions=parser.get_format_instructions())
+    chain = prompt | llm | parser
+    try:
+        return chain.invoke({'query': query})
+    except Exception:
+        return None
+
+
+question = 'What is the casting of Star Wars the first episode?'
+query = llm.invoke(question)
+print(prompt_query(query=query, schema=Casting, llm=llm))
