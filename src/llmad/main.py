c39e7ee8a273e3d0b4eee16682d9c3c16a09bc2e
Ahmed Ilyas <ahmed.n.ilyas@gmail.com>
2024-05-08 11:01:27 +0200

    use json parser

--- src/llmad/main.py
+++ src/llmad/main.py
@@ -1,11 +1,15 @@
 from nomad_simulations import Program
 
 from llmad.prompt_generator import PromptGenerator
-
+from langchain.output_parsers.json import SimpleJsonOutputParser
+from llmad.llm_model import llm
 
 prompt_generator = PromptGenerator(
     nomad_schema=Program.m_def,
     raw_files_paths=['/home/josepizarro/LLM-Hackathon-2024/tests/data/example.out'],
 )
-template = prompt_generator.generate()
+prompt = prompt_generator.generate()
+json_parser = SimpleJsonOutputParser()
+template = prompt | llm | json_parser
 llm_msg = template.invoke({'input': prompt_generator.raw_files_paths[0]})
+print(llm_msg)
