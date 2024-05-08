a9d58be1ec9a6cc4dced355d607b0a6b0a547c0c
Ahmed Ilyas <ahmed.n.ilyas@gmail.com>
2024-05-08 13:10:28 +0200

    update the promopt and use generator

--- src/llmad/main.py
+++ src/llmad/main.py
@@ -3,32 +3,30 @@ import time
 
 from nomad_simulations import Simulation, Program
 
-from llmad.prompt_generator import PromptGenerator
+from llmad.prompt_generator import PromptGenerator, PromptGeneratorInput
 from langchain.output_parsers.json import SimpleJsonOutputParser
 from llmad.llm_model import llm
 
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 test_file = '../../tests/data/example.out'
+test_file = '../../tests/data/Si2_crystal_VASP/OUTCAR'
 test_file_path = os.path.join(current_dir, test_file)
 
-prompt_generator = PromptGenerator(
-    nomad_schema=Simulation.m_def,
-    raw_files_paths=[test_file_path],
+data = PromptGeneratorInput(
+    nomad_schema=Program.m_def, raw_files_paths=[test_file_path], content=[]
 )
+prompt_generator = PromptGenerator(data=data)
+prompt_generator.read_raw_files_with_chunking()
 prompt = prompt_generator.generate()
 json_parser = SimpleJsonOutputParser()
-template = prompt | llm
-with open(os.path.join(current_dir, '../../test_llm.txt'), 'w') as file:
-    for i in range(10):
-        start_time = time.time()
-        try:
-            file_content = prompt_generator.read_raw_files(
-                prompt_generator.raw_files_paths
-            )
-            llm_msg = template.invoke({'input': file_content[0]})
-        except Exception:
-            print(i, 'exception catched!')
-        elapsed_time = time.time() - start_time
-        print(i, llm_msg, elapsed_time)
-        file.write(f'{str(elapsed_time)}, {llm_msg}')
+
+counter = 0
+archive = None
+for prompt in prompt_generator.generate():
+    template = prompt | llm | json_parser
+    print('chunk')
+    archive = template.invoke({'previous_archive': archive})
+    print(archive)
+
+print(archive)
