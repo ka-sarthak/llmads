d5e6fa24d414bf4d3420f53dd23c930f63909163
Ahmed Ilyas <ahmed.n.ilyas@gmail.com>
2024-05-08 11:11:59 +0200

    use json parser (#13)

--- src/llmad/utils.py
+++ src/llmad/utils.py
@@ -1,6 +1,3 @@
-from typing import List
-import re
-import json
 import warnings
 import magic
 
@@ -18,25 +15,3 @@ def identify_mime_type(file_path: str) -> str:
         warnings.warn(f'No file extension found for "{file_path}".')
 
     return mime_type
-
-
-def extract_json(message: str = '') -> List[dict]:
-    """Extracts JSON content from a string where JSON is embedded between ```json and ``` tags.
-
-    Parameters:
-        message (str): The text containing the JSON content.
-
-    Returns:
-        (list): A list of extracted JSON strings.
-    """
-    # Define the regular expression pattern to match JSON blocks
-    pattern = r'```\njson(.*?)```'
-
-    # Find all non-overlapping matches of the pattern in the string
-    matches = re.findall(pattern, message, re.DOTALL)
-
-    # Return the list of matched JSON strings, stripping any leading or trailing whitespace
-    try:
-        return [json.loads(match.strip()) for match in matches]
-    except Exception:
-        raise ValueError(f'Failed to parse: {message}')
