3c1a05b4cdf12fce6ea86bb9e47fbb305ef3a30a
Sarthak Kapoor <sarthakkapoor@hotmail.com>
2024-05-09 16:43:14 +0200

    Reorganize test data 2

--- tests/test_prompt_generator.py
+++ tests/test_prompt_generator.py
@@ -3,8 +3,8 @@ from llmad.prompt_generator import PromptGeneratorInput, PromptGenerator
 
 def test_prompt_generator():
     prompt_input = PromptGeneratorInput(
-        nomad_schema_path='tests/data/test_schema.archive.yaml',
-        raw_data_file_paths=['tests/data/Si2_crystal_VASP/OUTCAR'],
+        nomad_schema_path='tests/data/computation/test_schema.archive.yaml',
+        raw_data_file_paths=['tests/data/computation/Si2_crystal_VASP/OUTCAR'],
     )
     prompt_dict = PromptGenerator.generate(prompt_input)
     assert isinstance(prompt_dict['context_and_raw_data'], str)
