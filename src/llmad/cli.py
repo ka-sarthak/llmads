8852fb445367a99faf40e76e493fe3bd193ccbe7
Sarthak Kapoor <sarthakkapoor@hotmail.com>
2024-07-04 15:50:22 +0200

    Add CLI

--- src/llmad/cli.py
+++ src/llmad/cli.py
@@ -1,11 +1,29 @@
+import click
 from llmad.utils import get_input_data
 from llmad.config import Config
 from llmad.data_model import XRDSettings, XRDResult, XRayDiffraction
 from llmad.llm_model import ChatGroqLlamaStructured, HULlama
 
 
-def main():
-    config = Config('llmad.yaml')
+@click.command()
+@click.option(
+    '--config',
+    'config_path',
+    default='llmad.yaml',
+    help='Path of the YAML config file.',
+)
+@click.argument('subcommand', type=click.Choice(['run']))
+def run_cli(subcommand, config_path):
+    try:
+        if subcommand == 'run':
+            click.echo(run(config_path))
+    except Exception as e:
+        click.echo(f'ERROR: {e}')
+
+
+def run(config_path):
+    config = Config(config_path)
+    print(config.__dict__)
 
     if config.schema == 'XRDSettings':
         schema = XRDSettings
@@ -24,13 +42,14 @@ def main():
         raise ValueError(f'Invalid model "{config.llm_model}".')
 
     chunk = 1
-    input_data = get_input_data(config.chunking)
+    input_data = get_input_data(
+        config.test_file_path,
+        config.chunking,
+        config.chunk_size,
+        config.chunk_overlap,
+    )
     for response in model.generate_response(input_data):
         print(
             f'Chunk {chunk} processed. LLM response of type {type(response)}:\n{response}\n'
         )
         chunk += 1
-
-
-if __name__ == '__main__':
-    main()
